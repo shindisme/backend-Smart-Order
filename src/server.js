0585334d2c718c25f51cfa1a3904a029f6ae809f
Shindie <ntiendung04@gmail.com>
2026-01-06 04:38:53 +0700

    fixed server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://frontend-smart-order.vercel.app"
+    ],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
